5213ea34639964f7fe358111a96ac4bf0dd57446
gcount85 <100mgml@gmail.com>
2023-08-04 17:42:15 +0900

    fix : modify manifests

--- src/app.py
+++ src/app.py
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     # run() method of Flask class runs the application
     # on the local development server.
     # app.debug = True
-    app.run(host='localhost', port='5000')
+    app.run(host='localhost')
